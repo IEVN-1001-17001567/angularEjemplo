e14a6132929086a27eb2c252537debac144d8460
Rodrigo Gomez <rodrigogp4812@gmail.com>
2023-09-27 18:48:40 -0600

    '27/09/2023'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { CalculosComponent } from './formularios/calculos/calculos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    CalculosComponent
   ],
 
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
 
   providers: [],
--- src/app/formularios/operas-bas/operas-bas.component.ts
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -6,37 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./operas-bas.component.css']
 })
 export class OperasBasComponent {
-  
-  operacion:string='';
+    
+  funcion:string='';
   num1:string='';
   num2:string='';
   resultado:number=0;
 
-  switch (this.operacion) {
-    case sumar:
-      sumar():void{
+  calcular():void{
+    switch (this.funcion) {
+      case 'suma':
         this.resultado=parseInt(this.num1)+parseInt(this.num2);
-      }
-      break;
-    case this.restar:
-      resta():void{
+        break;
+
+      case 'resta':
         this.resultado=parseInt(this.num1)-parseInt(this.num2);
-      }
-      break;
-
-    case this.multi:
-      multiplicar():void{
-        this.resultado=parseInt(this.num1)*parseInt(this.num2);
-      }
-      break;
-
-    case this.divir:
-      dividir():void{
-        this.resultado=parseInt(this.num1)/parseInt(this.num2);
-      }
-      break;
+        break;
   
-    default:
-      break;
+      case 'multi':
+        this.resultado=parseInt(this.num1)*parseInt(this.num2);        
+        break;
+  
+      case 'divi':
+        this.resultado=parseInt(this.num1)/parseInt(this.num2);        
+        break;
+    
+      default:
+        break;
+    }
   }
 }
